31bf5611cde0501edc2cbaf2cac840d62c1a0b37
Jelena Alute <92847440+JelenaAlute@users.noreply.github.com>
2022-03-29 17:37:16 +0000

    Demo_piemers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 
 app = Flask('app')
@@ -9,6 +9,27 @@ app.config['JSON_AS_ASCII'] = False
 def index():
   return render_template("index.html")
 
+@app.route('/demo')
+def demo():
+  return render_template("demo.html")
+
+@app.route('/demoPoga', methods=['POST', 'GET'])
+def demoPoga():
+  if request.method == "GET":
+    with open("dati.txt", "r", encoding="utf-8") as f:
+      dati = f.read()
+    return dati
+  elif request.method == "POST":
+    ievade =request.json
+
+    with open("dati.txt", "a", encoding="utf-8") as f:
+      f.write(f"{ievade['datiY']}\n")
+   
+    return "OK"
+  else:
+    return "Kā tu te tiki?"
+    
+  
 @app.route('/noteikumi')
 def noteikumi():
   return render_template("noteikumi.html")
